cb6baf5ac0a382e5851d46e6bca0bef9c0c294c4
TheGoldenPorkchop <angelnavamusic@gmail.com>
2025-02-02 12:26:55 -0700

    Add project files.

---
+++ SayMyNameAgain/SayMyNameAgain.vb
@@ -0,0 +1,25 @@
+﻿'Angel Nava
+'Spring 2025
+'RCET2265
+'SayMyName
+
+Module SayMyNameAgain
+
+    Sub Main()
+        Dim userName As String
+
+        Console.WriteLine("Give me your name: ")
+        userName = Console.ReadLine()
+
+        If userName = "Emily" Then
+            Console.WriteLine("Greetings, Emily.")
+        ElseIf userName = "Joe" Then
+            Console.WriteLine("Ay! What it do, Joe.")
+        ElseIf userName = "Angel" Then
+            Console.WriteLine("Woah, that's the programmer")
+        Else
+            Console.WriteLine(vbNewLine & "Hello, " & userName)
+        End If
+    End Sub
+
+End Module
