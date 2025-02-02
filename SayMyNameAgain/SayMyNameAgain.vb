88a34e488e20c971d9d2b4ac6b2d6cb8f3fdfdb5
TheGoldenPorkchop <angelnavamusic@gmail.com>
2025-02-02 12:47:24 -0700

    Added program information header and a Do loop

--- SayMyNameAgain/SayMyNameAgain.vb
+++ SayMyNameAgain/SayMyNameAgain.vb
@@ -2,24 +2,33 @@
 'Spring 2025
 'RCET2265
 'SayMyName
+'https://github.com/TheGoldenPorkchop/SayMyNameAgain/blob/main/SayMyNameAgain/SayMyNameAgain.vb
 
+Option Explicit On
+Option Strict On
+Option Compare Text
 Module SayMyNameAgain
 
     Sub Main()
         Dim userName As String
+        Dim quit As Boolean = False
 
-        Console.WriteLine("Give me your name: ")
-        userName = Console.ReadLine()
-
-        If userName = "Emily" Then
-            Console.WriteLine("Greetings, Emily.")
-        ElseIf userName = "Joe" Then
-            Console.WriteLine("Ay! What it do, Joe.")
-        ElseIf userName = "Angel" Then
-            Console.WriteLine("Woah, that's the programmer")
-        Else
-            Console.WriteLine(vbNewLine & "Hello, " & userName)
-        End If
+        Do
+            Console.WriteLine("Enter Username: ")
+            userName = Console.ReadLine()
+            If userName = "Emily" Then
+                Console.WriteLine("Greetings, Emily.")
+                quit = True
+            ElseIf userName = "Joe" Then
+                Console.WriteLine("Ay! What it do, Joe.")
+                quit = True
+            ElseIf userName = "Angel" Then
+                Console.WriteLine("Woah, that's the programmer")
+                quit = True
+            Else
+                Console.WriteLine("Invalid user. Please try again." & vbNewLine)
+            End If
+        Loop Until quit = True
     End Sub
 
 End Module
